c72bb45bc558a4bf181a7d7c9abd3e2e2efd64cf
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-04 15:46:22 +0100

    [#160082601] Create API endpoint for fetching all foods item

---
+++ dummyServer/model/foods.js
@@ -0,0 +1,31 @@
+const foods = [
+  {
+    id: 1,
+    title: 'Jollof rice',
+    description: 'Jollof rice from Nigeria',
+    price: 5000,
+    imageUrl: 'https://google.meal/jollof',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  },
+  {
+    id: 2,
+    title: 'Semovita with Egusi soup',
+    description: 'sumptous home made semovita, highly nourishing',
+    price: 2500,
+    imageUrl: 'https://google.meal/semo',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  },
+  {
+    id: 3,
+    title: 'Pounded yam with Efo riro',
+    description: 'The king of all Africa\'s food',
+    price: 8000,
+    imageUrl: 'https://google.meal/jollof',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  },
+];
+
+export default foods;
--- dummyServer/route/v1/index.js
+++ dummyServer/route/v1/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import authRouter from './auth';
+import foodRouter from './food';
 
 const v1Router = express.Router();
 
-v1Router.use('/v1/', authRouter);
+v1Router.use(
+  '/v1/',
+  authRouter,
+  foodRouter
+);
 
 export default v1Router;
