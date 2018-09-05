f0843a684bd8041262a2d4a5aba12ea8e5cacaac
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-05 21:34:46 +0100

    [Feature #160082529] Write test for get all orders API endpoint

--- dummyServer/test/order.spec.js
+++ dummyServer/test/order.spec.js
@@ -76,3 +76,40 @@ describe('API endpoint POST /orders/', () => {
       });
   });
 });
+
+describe('API endpoint GET /orders/', () => {
+  it('it should successfully fetch all orders', (done) => {
+    chai.request(app)
+      .get('/api/v1/orders/')
+      .send()
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(res.body.message).to.equal('All orders fetched successfully');
+        expect(res.body.status).to.equal(200);
+        expect(res.body.success).to.equal(true);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('message');
+        res.body.should.have.property('orders');
+        expect(res.body.orders).to.be.an('array');
+        if (res.body.orders.length >= 1) {
+          res.body.orders[0].should.have.keys(
+            'orderId', 'food', 'totalPrice',
+            'customer', 'quantity', 'createdAt',
+            'updatedAt', 'deliveryAddress', 'status'
+          );
+          expect(res.body.orders[0].customer).to.be.an('object');
+          expect(res.body.orders[0].food).to.be.an('object');
+          res.body.orders[0].customer.should.have.keys(
+            'userId', 'fullname', 'email'
+          );
+          res.body.orders[0].food.should.have.keys(
+            'foodId', 'title', 'description',
+            'price'
+          );
+          expect(res.body.orders[0].food.price * res.body.orders[0].quantity)
+            .to.equal(res.body.orders[0].totalPrice);
+        }
+        done();
+      });
+  });
+});
