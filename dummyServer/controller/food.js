ab3a0db0e26c8eb032220993ae8649b498e412ed
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-06 16:19:59 +0100

    [#160082582] Create an API endpoint that fetches a specific food item

--- dummyServer/controller/food.js
+++ dummyServer/controller/food.js
@@ -17,4 +17,30 @@ export default class FoodController {
       foods
     });
   }
+
+  /**
+      * @description handles fetching of one food item
+      * @param {*} req - Incoming Request object
+      * @param {*} res - Incoming Message
+      * @returns {object} res - Route response
+      */
+  static getFood(req, res) {
+    const id = Number.parseInt(req.params.id, 10);
+    const food = foods.find(el => (el.id === id));
+    if (food) {
+      return res.status(200).send({
+        success: true,
+        status: 200,
+        message: 'Food was fetched successfully',
+        food
+      });
+    }
+    return res.status(404).send({
+      success: false,
+      status: 404,
+      error: {
+        message: `No Food matches the ID of ${id}`
+      }
+    });
+  }
 }
--- dummyServer/route/v1/food.js
+++ dummyServer/route/v1/food.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import FoodController from '../../controller/food';
+import { validateId } from '../../helpers/utils';
 
 
 const foodRouter = express.Router();
@@ -7,6 +8,11 @@ const foodRouter = express.Router();
 foodRouter.get(
   '/foods/',
   FoodController.getAllFoods
-);
+)
+  .get(
+    '/foods/:id',
+    validateId,
+    FoodController.getFood
+  );
 
 export default foodRouter;
